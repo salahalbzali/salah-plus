f07ac415da1ecbdda1308ac1c89961381dc221d7
SALAHPRMAGA <101624895+salahalbzali@users.noreply.github.com>
2026-06-04 14:40:49 +0300

    Add files via upload

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://salah-plus-l7lb.vercel.app/sitemap.xml',
+  }
+}
